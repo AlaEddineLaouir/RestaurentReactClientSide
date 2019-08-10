d7874ebfa438f6548476b88ef6d5dd18a277d33d
LAOUIR Ala Eddine <laouir.alaeddine@gmail.com>
2019-08-10 16:04:08 +0100

    blabla

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from "react";
 
 import Home from "./components/Pages/home";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
+import LoginForm from "./components/Authentication/LoginForm";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { isPattern } from "@babel/types";
 
 function App() {
   return (
     <Provider store={store}>
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Login" component={LoginForm} />
+        </Switch>
+      </Router>
     </Provider>
   );
 }
